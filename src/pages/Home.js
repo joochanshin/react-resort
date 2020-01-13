c130c03d6fb08aff6c2110dcbd1a7b867e7ae1c1
JooChan <14jshin@gmail.com>
2020-01-12 23:44:22 -0500

    cooments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
+import {Link} from "react-router-dom"
 
 export default function Home(){
   return <Hero>
   <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+  <Link to="/rooms" className = "btn-primary"></Link>
   </Banner>
-  /*1:05:23*/
   </Hero>;
 }
